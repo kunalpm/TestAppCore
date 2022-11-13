d50ed9e81b23e3c3604c2eca200422d8497b6224
kunalpm <118081674+kunalpm@users.noreply.github.com>
2022-11-13 14:45:49 +0000

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestAppCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
